22db0500ff665db1a12cca13b7baf98d2b0c00a8
JyriSem <jyrisem@gmail.com>
2024-11-04 16:06:25 +0200

    If Else Condition

--- src/DayOne.java
+++ src/DayOne.java
@@ -1,5 +1,16 @@
 public class DayOne {
     public static void main(String[] args) {
-        System.out.println("J");
+        int a = -5;
+        int b = -5;
+
+        if ((a < b) && (a > 0)) {
+            System.out.println("B is bigger than A, and A is positive");
+        } else if ((a > b) && (b > 0)) {
+            System.out.println("A is bigger than B, and B is positive");
+        } else if ((a == b) || (a < 0) && (b < 0)) {
+            System.out.println("A and B are either equal or both are negative");
+        } else {
+            System.out.println("No specific conditions were met");
+        }
     }
 }
